a0292ff02afe0ca8e98e03605939bc344ffffc08
PPTQ Quantum Qolbu <fitriyaninurfaizah467@gmail.com>
2022-08-24 11:48:26 +0700

    add pertemuan 2

---
+++ novice/02-02/4.1. if Statements/index.py
@@ -0,0 +1,10 @@
+# print("hello world")
+x = int(input("silahkan masukkan sebuah angka: "))
+if x < 0:
+    x = 0
+    print("replace dengan angka nol")
+ 
+else:
+    print("lainnya")
+
+print (x)
---
+++ novice/02-02/4.6. match Statements/index.py
@@ -0,0 +1,17 @@
+# def http_eror(status):
+#         match status:
+#             case 400:
+#                 return "Bad request"
+#             case 404:
+#                 return "Not found"
+#             case 418:
+#                 return "I'm a teapot"
+#             case _:
+#                 return "Something's wrong with the internet"
+
+# print(http_eror())
+
+match point:
+    case (0, 0):
+        print("Origin")
+    
